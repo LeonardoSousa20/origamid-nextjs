c2b3cf1195be5b4babd61c6d8bfd9396af3aeb23
Leonardo Sousa <leonardo.sousa7120@gmail.com>
2025-05-21 18:49:29 -0300

    remove code

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,13 +1,11 @@
-import Link from "next/link";
+import Link from 'next/link';
 
-export default async function Menu(){
-
-
-    return (
-        <ul id='menu' className='flex font-bold gap-8 p-4'>
-            <li>
-                <Link href='/'>Home</Link>
-            </li>
-        </ul>
-    )
+export default async function Menu() {
+  return (
+    <ul id="menu" className="flex font-bold gap-8 p-4">
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+    </ul>
+  );
 }
